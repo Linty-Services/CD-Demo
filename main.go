9fa779662682c5e3d92e9ae091d95a070e57e12b
vincent-louis <vincent.louis@linty-services.com>
2019-04-04 08:27:49 +0200

    Text Changed

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<body style='background-color: yellow'><h1>Hellow world !</h1></body>")
+	io.WriteString(w, "<body style='background-color: yellow'><h1>Hellow world Safran !</h1></body>")
 }
 
 func main() {
